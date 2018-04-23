ffac6f35f99271d720a0dfb55e963dbc28bfc1e8
Richard Wong <ricwtk@gmail.com>
2018-04-23 17:54:46 +0800

    set ui for note filtering screen

--- js/custom.js
+++ js/custom.js
@@ -199,25 +199,99 @@ new Vue({
   }
 })
 
+Vue.component("modal-sortnote", {
+  props: ["show", "value", "sortableFields", "groupableFields", "availableGroups"],
+  data: function () {
+    return {
+      filter: {
+        sort: "",
+        group: "",
+        show: []
+      }
+    }
+  },
+  computed: {
+    groupList: function () {
+      let groupIdx = this.groupableFields.indexOf(this.filter.group);
+      if (groupIdx < 0) return [];
+      else return this.availableGroups[groupIdx];
+    }
+  },
+  methods: {
+    hideMe: function () {
+      this.$emit("hide");
+      Vue.nextTick(() => {
+        this.filter = JSON.parse(JSON.stringify(this.value));
+      })
+    },
+    selectAllGroups: function () {
+      this.$refs.groupList.childNodes.forEach(g => g.selected = true);
+    },
+    updateFilter: function () {
+      this.$emit("input", {
+        sort: this.$refs.sortbyList.selectedOptions[0].textContent,
+        group: this.$refs.groupbyList.selectedOptions[0].textContent,
+        show: Array.from(this.$refs.groupList.selectedOptions).map(el => el.textContent)
+      });
+      this.hideMe();
+    }
+  },
+  mounted: function () {
+    this.filter = JSON.parse(JSON.stringify(this.value));
+  },
+  template:`
+  <div :class="{ 'modal': true, 'active': show }">
+    <div class="modal-overlay"></div>
+    <div class="modal-container">
+      <div class="modal-header">Sort notes</div>
+      <div class="modal-body">
+        <input-header title="Sort by" :removable="false" label-for=""></input-header>
+        <select class="form-select" ref="sortbyList" v-model="filter.sort">
+          <option></option>
+          <option v-for="sf in sortableFields">{{ sf }}</option>
+        </select>
+        <input-header title="Group by" :removable="false" label-for=""></input-header>
+        <select class="form-select" ref="groupbyList" v-model="filter.group">
+          <option></option>
+          <option v-for="gf in groupableFields">{{ gf }}</option>
+        </select>
+        <input-header title="Show group(s)" :removable="false" label-for=""></input-header>
+        <button class="btn col-12 my-1" :disabled="groupList.length<1" @click="selectAllGroups">Select all</button>
+        <select class="form-select" size="5" multiple :disabled="groupList.length<1" ref="groupList" v-model="filter.show">
+          <option v-for="ag in groupList">{{ ag }}</option>
+        </select>
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary" @click="updateFilter">Filter</button>
+        <button class="btn" @click="hideMe">Cancel</button>
+      </div>
+    </div>
+  </div>
+  `
+})
+
 Vue.component("search-bar", {
   data: function () {
     return {
-      hide: true
+      hidden: true
     }
   },
   methods: {
     toggle: function () {
-      this.hide = !this.hide;
+      this.hidden = !this.hidden;
     },
     show: function () {
-      this.hide = true;
+      this.hidden = false;
+    },
+    hide: function () {
+      this.hidden = true;
     }
   },
   template: `
-  <div :class="{ 'form-input': !hide, 'search-bar': true }" @click="toggle">
-    <span :class="{ 'mdi': true, 'mdi-magnify': true, 'form-icon': !hide, 'mdi-24px': true }" @click="show"></span>
-    <input v-show="!hide" type="text" class="invisible-form-input" @click.stop>
-    <span v-show="!hide" class="form-icon mdi mdi-close tooltip tooltip-bottom" data-tooltip="clear search entry"></span>
+  <div :class="{ 'form-input': !hidden, 'search-bar': true }">
+    <span :class="{ 'mdi': true, 'mdi-magnify': true, 'form-icon': !hidden, 'mdi-24px': true }" @click="show"></span>
+    <input v-show="!hidden" type="text" class="invisible-form-input" @click.stop>
+    <span v-show="!hidden" class="form-icon mdi mdi-close tooltip tooltip-bottom" data-tooltip="clear search entry" @click="hide"></span>
   </div>
   `
 })
@@ -568,6 +642,12 @@ new Vue({
     stat: stat,
     tickedFiles: [],
     stored: stored,
+    showFilter: false,
+    filter: {
+      sort: "",
+      group: "",
+      show: []
+    },
     openedFile: {},
     fileToOpen: {},
     elToOpen: null,
@@ -580,9 +660,41 @@ new Vue({
   computed: {
     hasUnsaved: function () {
       return JSON.stringify(this.unsaved) != JSON.stringify(this.openedFile);
+    },
+    sortableFields: function () { 
+      return Array.from( 
+        new Set( 
+          this.stored.noteList.map(el =>  
+            Object.keys(el).filter(k => 
+              ['created', 'modified'].includes(k) || 
+              ( el[k].type && ["single", "datetime"].includes(el[k].type) )
+            ) 
+          ).reduce((acc, el) => acc.concat(el), []) 
+        ) 
+      ); 
+    }, 
+    groupableFields: function () { 
+      return Array.from( 
+        new Set( 
+          this.stored.noteList.map(el =>  
+            Object.keys(el).filter(k =>  
+              el[k].type && ["tags"].includes(el[k].type) 
+            ) 
+          ).reduce((acc, el) => acc.concat(el), []) 
+        ) 
+      ); 
+    },
+    availableGroups: function () {
+      return this.groupableFields.map(field => 
+        this.stored.noteList.map(el => el[field] ? el[field].content : [])
+        .reduce((acc, el) => acc.concat(el), [])
+      );
     }
   },
   methods: {
+    // getGroups: function (field) {
+    //   this.availableGroups = this.stored.noteList.map(el => el[field] ? el[field].content: []).reduce((acc, el) => acc.concat(el), []);
+    // }, 
     addTick: function (fileId) {
       console.log("ticking", fileId);
       this.tickedFiles.push(fileId);
