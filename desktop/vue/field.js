d616f6e6a0bec24ea29bbc228ab3af89a03d0fa4
Richard Wong <ricwtk@gmail.com>
2018-05-05 21:29:05 +0800

    fix problem with input header/divider that covers the element underneath

--- desktop/vue/field.js
+++ desktop/vue/field.js
@@ -313,12 +313,12 @@ let _tags = {
     ></input-header>
 
     <div v-if="editable">
-      <input class="form-input" type="text" :id="'value-' + title" :placeholder="title" @keydown="updateTags">
-      <div class="my-2"></div>
       <span class="chip" v-for="tag in value">
         {{ tag }}&nbsp;
         <span class="mdi mdi-close" @click="removeTag(tag)"></span>
       </span>
+      <div class="my-2"></div>
+      <input class="form-input" type="text" :id="'value-' + title" :placeholder="title" @keydown="updateTags">
     </div>
       
     <div v-else class="display-input">
